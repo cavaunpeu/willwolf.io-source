7d75616f2e3d2f61f1c9d54a4ce41c4ecb6168d5
William Wolf <williamabrwolf@gmail.com>
2020-04-12 09:58:35 -0400

    delete extraneous character

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'https://cavaunpeu.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
